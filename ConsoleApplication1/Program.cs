24d95da0b43f1b17af6e6feeaf42a916b7682929
shane <shane@SMENCHIONS-PC>
2017-02-07 17:48:43 -0800

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+
+    public static class IPIdentity
+    {
+        /// <summary>
+        /// Given an IP address in string form, determine if is it public or private. V4 or V6
+        /// </summary>
+        /// <param name="IPAddress">ip address string in form XXX.XXX.XXX.XXX or XXXX:XXXX.....:XXXX</param>
+        /// <returns>True if public, false if private, null if invalid</returns>
+        public static Boolean? isPublic(String IPAddress)
+        {
+            // basic check for valid input
+            if (String.IsNullOrEmpty(IPAddress))
+                return null;
+            //check ipv6
+            string[] components = IPAddress.Split(':');
+            if(components.Length > 1)
+            {
+                if (components[0].ToUpper().StartsWith("FD"))
+                    return false;
+                return true;
+            }
+            //ipv4
+            components = IPAddress.Split('.');
+            // basic check for IP validity
+            if (components.Length != 4)
+                return null;
+            if (components[0] == "10")
+                return false;
+            if (components[0] == "192" && components[1] == "168")
+                return false;
+            //only parse ints when needed for efficiency
+            int classB = int.Parse(components[1]);
+            if (components[0] == "172" && (classB >=16 && classB <= 31))
+                return false;
+            return true;
+        }
+    } 
+
+    class Program
+    {
+        static void Main(string[] args)
+        {           
+            Console.WriteLine(IPIdentity.isPublic("123.456.789.000"));
+            Console.WriteLine(IPIdentity.isPublic("192.168.789.000"));
+            Console.WriteLine(IPIdentity.isPublic("10.789.000"));
+            Console.WriteLine(IPIdentity.isPublic("fd20:4a55:d0f1:94b7:xxxx:xxxx:xxxx:xxxx"));
+        }
+    }
+}
---
+++ ConsoleApplication1Tests/IPIdentityTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1.Tests
+{
+    [TestClass()]
+    public class IPIdentityTests
+    {
+        [TestMethod()]
+        public void isPublicTest()
+        {
+            Assert.IsTrue(IPIdentity.isPublic("badipaddress") == null);
+            Assert.IsTrue(IPIdentity.isPublic("123.255.255.000") == true);
+            Assert.IsTrue(IPIdentity.isPublic("192.168.254.000") == false);
+            Assert.IsTrue(IPIdentity.isPublic("10.789.1.255") == false);
+            Assert.IsTrue(IPIdentity.isPublic("172.16.1.212") == false);
+            Assert.IsTrue(IPIdentity.isPublic("172.15.1.212") == true);
+            Assert.IsTrue(IPIdentity.isPublic("172.31.1.212") == false);
+            Assert.IsTrue(IPIdentity.isPublic("172.32.1.212") == true);
+            Assert.IsTrue(IPIdentity.isPublic("fd20:4a55:d0f1:94b7:xxxx:xxxx:xxxx:xxxx") == false);
+            Assert.IsTrue(IPIdentity.isPublic("2001:db8::211:22ff:fe33:4455") == true);
+            Assert.IsTrue(IPIdentity.isPublic("2001:db8:0000:211:22ff:fe33:4455:0000") == true);
+
+        }
+    }
+}
